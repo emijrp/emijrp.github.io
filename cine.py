6e37d4a48b87d024c62a6f7ec664a95b6c5248ba
emijrp <emijrp@gmail.com>
2015-10-15 17:45:01 +0200

    stats

--- cine.py
+++ cine.py
@@ -20,6 +20,24 @@ import re
 import time
 import urllib2
 
+countries = {
+    u'Unión Soviética': u'ZY', 
+}
+
+def cleanFilmDirector(filmdirector):
+    if '(AKA' in filmdirector:
+        filmdirector = filmdirector.split(' (AKA')[0]
+    if '(Creator' in filmdirector:
+        filmdirector = filmdirector.split(' (Creator')[0]
+    return filmdirector
+
+def getFilmCountryLink(filmcountry):
+    filmcountrylink = 'http://www.filmaffinity.com/es/advsearch.php?stext=&stype[]=title&country=%s&genre=&fromyear=&toyear=' % (countries[filmcountry])
+    return filmcountrylink
+    
+def getFilmDirectorLink(filmdirector):
+    return 'http://www.filmaffinity.com/es/search.php?stype=director&sn&stext=%s' % (re.sub(' ', '+', filmdirector))
+
 def getFilmYearLink(filmyear):
     filmyearlink = ''
     if filmyear < 1900:
@@ -30,10 +48,14 @@ def getFilmYearLink(filmyear):
 
 def main():
     films = []
-    countries = {
-        u'Unión Soviética': u'ZY', 
-    }
+    
     userid = 397713
+    
+    #stats
+    statsdirector = {}
+    statscountry = {}
+    statsyear = {}
+    statsdecade = {}
     
     for page in range(1, 100):
         faurl = 'http://www.filmaffinity.com/es/userratings.php?user_id=%s&p=%s&orderby=4' % (userid, page)
@@ -52,17 +74,24 @@ def main():
         for rating in m:
             ratings.append(rating)
         
-        m = re.findall(ur'(?im)<div class="mc-title">\s*<a href="/es/film(\d+)\.html">([^<>]*?)</a>\s*\((\d+?)\)\s*<img src="/imgs/countries/([^<>]+)\.jpg" title="([^<>]+?)">\s*</div>\s*<div class="mc-director">\s*<a href="/es/search\.php\?stype=director&amp;sn&amp;stext=[^<>]+?">([^<>]+?)</a>', html) #no cerrar con </div> por si el campo director tiene más de un director
+        m = re.findall(ur'(?im)<div class="mc-title">\s*<a href="/es/film(\d+)\.html">([^<>]*?)</a>\s*\((\d+?)\)\s*<img src="/imgs/countries/([^<>]+)\.jpg" title="([^<>]+?)">\s*</div>\s*<div class="mc-director">\s*<a href="/es/search\.php\?stype=director&amp;sn&amp;stext=[^<>]+?">([^<>]+?)</a>', html) #no cerrar con </div> por si el campo director tiene más de un director, solo coge el primero (mejorar?)
         c = 0
         for film in m:
             filmid = film[0].strip()
             filmtitle = film[1].strip()
             filmyear = int(film[2].strip())
+            filmdecade = filmyear / 10
             filmcountryid = film[3].strip()
             filmcountry = film[4].strip()
             filmdirector = film[5].strip()
             films.append([filmtitle, filmyear, filmcountry, filmdirector, filmid, ratings[c]])
             countries[filmcountry] = filmcountryid
+            
+            statsyear[filmyear] = statsyear.has_key(filmyear) and statsyear[filmyear] + 1 or 1
+            statsdecade[filmdecade] = statsdecade.has_key(filmdecade) and statsdecade[filmdecade] + 1 or 1
+            statscountry[filmcountry] = statscountry.has_key(filmcountry) and statscountry[filmcountry] + 1 or 1
+            statsdirector[filmdirector] = statsdirector.has_key(filmdirector) and statsdirector[filmdirector] + 1 or 1
+
             c += 1
         
         time.sleep(1)
@@ -88,10 +117,7 @@ def main():
         if filmtitle.endswith(' (TV)'):
             filmtitle = filmtitle.split(' (TV)')[0]
         
-        if '(AKA' in filmdirector:
-            filmdirector = filmdirector.split(' (AKA')[0]
-        if '(Creator' in filmdirector:
-            filmdirector = filmdirector.split(' (Creator')[0]
+        filmdirector_ = cleanFilmDirector(filmdirector)
         
         if 'Holanda' in filmcountry:
             filmcountry = filmcountry.split(' (Holanda)')[0]
@@ -100,7 +126,8 @@ def main():
         
         years.add(filmyear)
         c += 1
-        filmdirectorlink = 'http://www.filmaffinity.com/es/search.php?stype=director&sn&stext=%s' % (re.sub(' ', '+', filmdirector))
+        filmcountrylink = getFilmCountryLink(filmcountry)
+        filmdirectorlink = getFilmDirectorLink(filmdirector)
         filmyearlink = getFilmYearLink(filmyear)
         row = u"""
     <tr>
@@ -108,9 +135,9 @@ def main():
         <td sorttable_customkey="%s"><i><a href="http://www.filmaffinity.com/es/film%s.html">%s</a></i></td>
         <td><a href="%s">%s</a></td>
         <td><a href="%s">%s</a></td>
-        <td><a href="http://www.filmaffinity.com/es/advsearch.php?stext=&stype[]=title&country=%s&genre=&fromyear=&toyear=">%s</a></td>
+        <td><a href="%s">%s</a></td>
         <td>%s</td>
-    </tr>\n""" % (c, customkey, filmid, filmtitle, filmdirectorlink, filmdirector, filmyearlink, filmyear, countries[filmcountry], filmcountry, filmrating)
+    </tr>\n""" % (c, customkey, filmid, filmtitle, filmdirectorlink, filmdirector_, filmyearlink, filmyear, filmcountrylink, filmcountry, filmrating)
         rows.append(row)
     
     #add missing years
@@ -152,7 +179,7 @@ def main():
     </tr>\n""" % (y, x, y)
             rows.append(row)
     
-    #save table
+    #print table
     table = u"\n<script>sorttable.sort_alpha = function(a,b) { return a[0].localeCompare(b[0], 'es'); }</script>\n"
     table += u'\n<table class="wikitable sortable" style="text-align: center;">\n'
     table += u"""
@@ -172,6 +199,38 @@ def main():
     f.close()
     f = open('cine.html', 'w')
     html = u'%s<!-- tabla completa -->%s<!-- /tabla completa -->%s' % (html.split(u'<!-- tabla completa -->')[0], table, html.split(u'<!-- /tabla completa -->')[1])
+    f.write(html.encode('utf-8'))
+    f.close()
+    
+    #print stats
+    statsyear_list = [[y, x] for x, y in statsyear.items()]
+    statsyear_list.sort()
+    statsyear_list.reverse()
+    
+    statsdecade_list = [[y, x] for x, y in statsdecade.items()]
+    statsdecade_list.sort()
+    statsdecade_list.reverse()
+    
+    statsdirector_list = [[y, x] for x, y in statsdirector.items()]
+    statsdirector_list.sort()
+    statsdirector_list.reverse()
+    
+    statscountry_list = [[y, x] for x, y in statscountry.items()]
+    statscountry_list.sort()
+    statscountry_list.reverse()
+    
+    stats = u"<ul>\n"
+    stats += u"<li>Por <b>años</b>: %s</a>\n" % (', '.join([u'<a href="%s">%s</a> (%s)' % (getFilmYearLink(x), x, y) for y, x in statsyear_list]))
+    stats += u"<li>Por <b>décadas</b>: %s</a>\n" % (', '.join([u'%s0 (%s)' % (x, y) for y, x in statsdecade_list]))
+    stats += u"<li>Por <b>director</b>: %s</a>\n" % (', '.join([u'<a href="%s">%s</a> (%s)' % (getFilmDirectorLink(x), cleanFilmDirector(x), y) for y, x in statsdirector_list]))
+    stats += u"<li>Por <b>país</b>: %s</a>\n" % (', '.join([u'<a href="%s">%s</a> (%s)' % (getFilmCountryLink(x), x, y) for y, x in statscountry_list]))
+    stats += u"</ul>\n"
+    
+    f = open('cine.html', 'r')
+    html = unicode(f.read(), 'utf-8')
+    f.close()
+    f = open('cine.html', 'w')
+    html = u'%s<!-- stats -->%s<!-- /stats -->%s' % (html.split(u'<!-- stats -->')[0], stats, html.split(u'<!-- /stats -->')[1])
     f.write(html.encode('utf-8'))
     f.close()
 
