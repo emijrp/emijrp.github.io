93035a543f8840067b2fcc73209db04c654284dd
emijrp <emijrp@gmail.com>
2016-07-09 22:11:30 +0200

    enlace buscar

--- biblioteca.py
+++ biblioteca.py
@@ -84,8 +84,9 @@ def main():
         <td sorttable_customkey="%s"><i><a href="https://www.goodreads.com%s">%s</a></i></td>
         <td><a href="https://www.goodreads.com%s">%s</a></td>
         <td><a href="https://en.wikipedia.org/w/index.php?title=Special:BookSources&isbn=%s">%s</a></td>
+        <td><a href="https://www.google.es/?gws_rd=ssl#q=%s">Buscar</a></td>
         <!--<td>...</td>-->
-    </tr>\n""" % (bookc, customkey, bookprops['bookurl'], booktitle, bookprops['authorurl'], bookprops['author'], bookprops['isbn13'], bookprops['isbn13'])
+    </tr>\n""" % (bookc, customkey, bookprops['bookurl'], booktitle, bookprops['authorurl'], bookprops['author'], bookprops['isbn13'], bookprops['isbn13'], re.sub(' ', '+', booktitle))
         bookrows.append(row)
 
     #print table
@@ -97,6 +98,7 @@ def main():
         <th class="sorttable_alpha">Título</th>
         <th class="sorttable_alpha">Autoría</th>
         <th class="sorttable_alpha">ISBN</th>
+        <th class="sorttable_alpha">Buscar</th>
         <!--<th class="sorttable_numeric">Puntos</th>-->
     </tr>"""
     booktable = table
