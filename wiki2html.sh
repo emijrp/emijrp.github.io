e68e73c1cfe8b84bbb4a7ac004bb7c3d842227e5
emijrp <emijrp@gmail.com>
2016-08-25 18:17:37 +0200

    batch script

---
+++ wiki2html.sh
@@ -0,0 +1,22 @@
+
+python wiki2html.py --all
+
+cd bio
+python ../wiki2html.py --all
+cd ..
+
+cd cine
+python ../wiki2html.py --all
+cd ..
+
+cd fil
+python ../wiki2html.py --all
+cd ..
+
+cd geo
+python ../wiki2html.py --all
+cd ..
+
+cd vuelta-a-espana
+python ../wiki2html.py --all
+cd ..
